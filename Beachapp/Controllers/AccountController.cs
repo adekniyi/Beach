2663be7b0013be8cbbc739eda613dc086abd6ecc
Adek Niyi <adekniyi@gmail.com>
2021-01-22 01:14:04 -0800

    setting up register and log in details

---
+++ Beachapp/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Beachapp.Data;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Beachapp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private SignInManager<ApplicationUser> _signInManager;
+        private Microsoft.AspNetCore.Identity.UserManager<ApplicationUser> _userManager;
+
+        public AccountController(Microsoft.AspNetCore.Identity.UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
+
+          // GET: /Account/Register
+        //[AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ Beachapp/Data/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace Beachapp.Data
+{
+         public class ApplicationUser : IdentityUser
+    {
+       
+    }
+}
